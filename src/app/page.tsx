4d23e9df8458c54a3a0025d22e33acd021824b29
Jokeronii <tendavid2@gmail.com>
2025-06-10 18:57:34 +0700

    chore: install shadcn/ui and setup base config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import { auth } from '@/lib/auth';
+
+export default async function Home() {
+  const session = await auth();
+
+  if (!session) {
+    return (
+      <main>
+        <h1>Not signed in</h1>
+        <p>please sign in</p>
+      </main>
+    );
+  }
+
   return (
     <main>
-      <h1>Home</h1>
+      <h1>{JSON.stringify(session)}</h1>
     </main>
   );
 }
