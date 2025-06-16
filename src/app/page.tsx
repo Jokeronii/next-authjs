280b71e83e9da8e33abae851c418acc8e56472c9
Jokeronii <tendavid2@gmail.com>
2025-06-16 20:01:09 +0700

    fix:stash error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { auth } from '@/auth';
-import { SignInForm } from '@/components/SignInForm';
+import SignInButton from '@/components/SignInButton';
+import SignOutButton from '@/components/SignOutButton';
 
 export default async function Home() {
   const session = await auth();
@@ -7,7 +8,7 @@ export default async function Home() {
   if (!session?.user) {
     return (
       <main className="w-screen h-dvh grid grid-rows-2 place-items-center">
-        <SignInForm />
+        <SignInButton />
       </main>
     );
   }
@@ -16,6 +17,11 @@ export default async function Home() {
     <main className="grid grid-cols-3">
       <div>
         <h1>main content</h1>
+        <ul>
+          <li>{session.user.name}</li>
+          <li>{session.user.email}</li>
+        </ul>
+        <SignOutButton />
       </div>
     </main>
   );
--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -6,7 +6,7 @@ import { Button } from './ui/button';
 export default function () {
   return (
     <>
-      <Button onClick={() => Login()}>Sign In</Button>
+      <Button onClick={() => Login()}>Sign In with Google</Button>
     </>
   );
 }
